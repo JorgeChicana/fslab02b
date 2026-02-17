e6ddd9f941d5f50eb76dedc8931c2b8dbd9f656b
JorgeChicana <desarrollochicana@gmail.com>
2026-02-17 15:50:09 -0500

    Agregar Websocket

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,43 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Servidor WebSocket activo");
+});
+
+// Evento de conexión
+io.on("connection", (socket) => {
+  console.log("Cliente conectado:", socket.id);
+
+  socket.emit("mensaje", "Bienvenido al servidor en tiempo real");
+
+  socket.on("mensaje-cliente", (data) => {
+    console.log("Mensaje recibido:", data);
+    io.emit("mensaje", data); // enviar a todos
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Cliente desconectado");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
+
+
+/*
+const express = require("express");
 const os = require("os");
 const { Pool } = require("pg");
 
@@ -38,3 +77,4 @@ app.get("/db-test", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en ${PORT}`));
+*/
